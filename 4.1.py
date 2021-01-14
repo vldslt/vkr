47c62de099353b0b87f64e9d183b6b1b95335c4a
Salat <76412243+vldslt@users.noreply.github.com>
2021-01-14 19:02:51 +0300

    dz 4
    есть несколько вопросов, будет очень здорово если оставите комментарии здесь

---
+++ 4.1.py
@@ -0,0 +1,20 @@
+'''
+Реализовать скрипт, в котором должна быть предусмотрена
+ функция расчета заработной платы сотрудника. В расчете
+ необходимо использовать формулу:
+ (выработка в часах*ставка в час) + премия.
+ Для выполнения расчета для конкретных значений необходимо
+ запускать скрипт с параметрами.
+'''
+
+from sys import argv
+
+name, time, salary, bonus = argv
+try:
+    time = int(time)
+    salary = int(salary)
+    bonus = int(bonus)
+    res = time * salary + bonus
+    print(f'зп сотрудника  {res}')
+except ValueError:
+    print('Not a number')
