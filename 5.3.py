dfc04f9dd917647e6a0dcbe26e76031691771b02
Salat <76412243+vldslt@users.noreply.github.com>
2021-01-18 18:56:04 +0300

    урок 5 дз
    задания 6 и 7 не смог сделать

---
+++ 5.3.py
@@ -0,0 +1,31 @@
+'''
+Создать текстовый файл (не программно),
+построчно записать фамилии сотрудников и величину их окладов.
+Определить, кто из сотрудников имеет оклад менее 20 тыс.,
+вывести фамилии этих сотрудников.
+Выполнить подсчет средней величины дохода сотрудников.
+'''
+
+firm = {'Коля Жопин': 17000, 'Изя Камушкин': 21000, 'Сержант Петров': 19000, 'Владимир Путин': 15000}
+try:
+    file_obj = open("dz53.txt", 'w')
+    for last_name, salary in firm.items():
+        file_obj.write(last_name + ':' + str(salary) + "\n")
+except IOError:
+    print("Произошла ошибка ввода-вывода!")
+finally:
+    file_obj.close()
+summa = 0
+count = 0
+persons = []
+with open("dz53.txt", "r") as file_obj:
+    for line in file_obj:
+        print(line, end="")
+        tokens = line.split(':')
+        if int(tokens[1]) <= 20000:
+            persons.append(tokens[0])
+        summa += int(tokens[1])
+        count += 1
+result = summa / count
+print(f"получают меньше 20к: {persons}")
+print(f"средняя зп: {result}")
